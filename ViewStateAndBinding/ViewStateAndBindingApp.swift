5d0b8281985d62ef7e133d1c0ec05ab00871230a
Mark Nair <marknair@me.com>
2023-04-17 09:13:08 -0500

    Initial Commit

---
+++ ViewStateAndBinding/ViewStateAndBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewStateAndBindingApp.swift
+//  ViewStateAndBinding
+//
+//  Created by Mark Nair on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewStateAndBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
