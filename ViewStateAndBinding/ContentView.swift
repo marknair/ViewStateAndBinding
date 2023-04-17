ac54f82de277337f4db303ec8f20b6bf0ae95d09
Mark Nair <marknair@me.com>
2023-04-17 10:10:14 -0500

    First changes

--- ViewStateAndBinding/ContentView.swift
+++ ViewStateAndBinding/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var swiftyColor: Color = .red
+    @State private var swiftyOpacity: Double = 0.7
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            SwiftyControls(swiftyColor: $swiftyColor, swiftyOpacity: $swiftyOpacity)
+        
+
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .padding(25)
+                .foregroundColor(.white)
+                .opacity(swiftyOpacity)
+                .background(swiftyColor)
+                .cornerRadius(50)
         }
         .padding()
     }
@@ -22,5 +34,18 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SwiftyControls: View {
+    @Binding var swiftyColor: Color
+    @Binding var swiftyOpacity: Double
+    
+    var body: some View {
+        VStack {
+            ColorPicker("Swifty Color", selection: $swiftyColor)
+            Slider(value: $swiftyOpacity, in: 0...1)
+                .accentColor(swiftyColor)
+        }
     }
 }
